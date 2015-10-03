d88d5a6532e5198ea307f27590384fe683ee05e8
0mkara <official.greymatter@gmail.com>
2015-10-04 00:58:45 +0530

    Add things slowly

--- public/js/coinpunk/models/onename.js
+++ public/js/coinpunk/models/onename.js
@@ -1,7 +1,7 @@
-var onenameAPI = new Test();
 coinpunk.Onename = function() {
 	/*var onenameAPI = new OnenameAPI.Test();
 	onenameAPI.hello();*/
+	Test.hello();
 	this.hello = function() {
 		console.log("Hello world");
 		return;
