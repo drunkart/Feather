cd6ca82528718294addf5ef3a9b469090a3d249f
0mkara <official.greymatter@gmail.com>
2015-10-04 16:58:58 +0530

    Add things one by one

--- public/js/coinpunk/models/onename.js
+++ public/js/coinpunk/models/onename.js
@@ -1,5 +1,6 @@
-var onename = new Bitcoin.OnenameAPI();
-	OnenameClient = onename.OnenameClient
 coinpunk.Onename = function() {
+	this.network = coinpunk.config.network || 'prod';
+	var onename = new Bitcoin.OnenameAPI();
+	var OnenameClient = onename.OnenameClient;
 	onenameClient = new OnenameClient('e6ae48efb4a458514b70c026707b2a99', 'f7fe13045c84ea4adc5a734d22d4d4463e4a59b2a9510bc9df0aaf3cd9975dbb');
 };
