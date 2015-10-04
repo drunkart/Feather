35ce24cfcbe3b15d4b2fb1d4155ab8cd23fe0b96
0mkara <official.greymatter@gmail.com>
2015-10-04 16:07:41 +0530

    Add things one by one

--- public/js/coinpunk/models/onename.js
+++ public/js/coinpunk/models/onename.js
@@ -1,5 +1,5 @@
-var Onename = require('./lib/onenamejs')
+var onenameAPI = require('onename-api'),
+	OnenameClient = onenameAPI.OnenameClient
 coinpunk.Onename = function() {
-	onenameClient = new Onename.OnenameAPI.OnenameClient('e6ae48efb4a458514b70c026707b2a99', 'f7fe13045c84ea4adc5a734d22d4d4463e4a59b2a9510bc9df0aaf3cd9975dbb');
-	onenameClient.hello();
+	onenameClient = new OnenameClient('e6ae48efb4a458514b70c026707b2a99', 'f7fe13045c84ea4adc5a734d22d4d4463e4a59b2a9510bc9df0aaf3cd9975dbb');
 };
