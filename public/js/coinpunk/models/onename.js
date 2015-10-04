eb2972d61c065eb157e882e8401c0e63c558831c
0mkara <official.greymatter@gmail.com>
2015-10-04 17:44:59 +0530

    Add things one by one and it works

--- public/js/coinpunk/models/onename.js
+++ public/js/coinpunk/models/onename.js
@@ -3,6 +3,6 @@ coinpunk.Onename = function() {
 	var Client = new Bitcoin.OnenameAPI.OnenameClient('e6ae48efb4a458514b70c026707b2a99', 'f7fe13045c84ea4adc5a734d22d4d4463e4a59b2a9510bc9df0aaf3cd9975dbb');
 
 	this.hello = function(email) {
-		console.log("Hello %s", email)
+		Client.hello(email)
 	};
 };
