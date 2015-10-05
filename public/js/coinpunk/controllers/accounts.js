9da361951a73a4bfdc8fd1b9dc2aaf103a768b1f
0mkara <official.greymatter@gmail.com>
2015-10-06 02:04:27 +0530

    Working with request

--- public/js/coinpunk/controllers/accounts.js
+++ public/js/coinpunk/controllers/accounts.js
@@ -31,6 +31,7 @@ coinpunk.controllers.Accounts.prototype.emailSearch = function(email) {
         emailExists = null
     $.get('/api/Onename/searchUser', body, function(response) {
         emailExists = response.emailExists
+        console.log(emailExists)
     })
     console.log(emailExists)
     if (emailExists == true)
