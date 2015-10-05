61b3481a7e9a642d81263ed803f5f26d68d5e1e1
0mkara <official.greymatter@gmail.com>
2015-10-05 10:51:34 +0530

    emailSearch works

--- public/js/coinpunk/models/onename.js
+++ public/js/coinpunk/models/onename.js
@@ -1,14 +1,14 @@
 coinpunk.Onename = function() {
 	this.network = coinpunk.config.network || 'prod';
-	this.OnenameClient = Bitcoin.OnenameAPI.OnenameClient;
-	this.AppID = 'e6ae48efb4a458514b70c026707b2a99';
-	this.AppKey = 'f7fe13045c84ea4adc5a734d22d4d4463e4a59b2a9510bc9df0aaf3cd9975dbb';
-	this.onenameClient = new this.OnenameClient(this.AppID, this.AppKey);
+	this._OnenameClient = Bitcoin.OnenameAPI.OnenameClient;
+	this._AppID = 'e6ae48efb4a458514b70c026707b2a99';
+	this._AppKey = 'f7fe13045c84ea4adc5a734d22d4d4463e4a59b2a9510bc9df0aaf3cd9975dbb';
+	this._onenameClient = new this._OnenameClient(this._AppID, this._AppKey);
 
 	this.findEmail = function(email) {
 		var usernames = ['fredwilson']
-		this.onenameClient.getUsers(usernames, function(err, users) {
-			console.log("Error %s", err)
+		this._onenameClient.getUsers(usernames, function(err, users) {
+			console.log(err)
 			console.log("Data %s", users)
 		})
 	};
