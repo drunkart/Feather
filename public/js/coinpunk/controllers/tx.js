accc0e651c505fff582e015998ec93c0aec6c5a6
0mkara <official.greymatter@gmail.com>
2015-10-11 18:31:13 +0530

    Try to find Comment implementation process with more logging

--- public/js/coinpunk/controllers/tx.js
+++ public/js/coinpunk/controllers/tx.js
@@ -146,7 +146,6 @@ coinpunk.controllers.Tx.prototype.create = function() {
       coinpunk.router.route('signout');
     } else {
       $.post('/api/tx/send', {tx: rawtx}, function(resp) {
-        console.log(resp)
         console.log(resp.hash)
         if(resp.error) {
           coinpunk.wallet.revertTx();
